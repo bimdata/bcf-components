b48455f5ea7c9b891d37d9a27748e3f88ee5e626
NicolasRichel <nicolas@bimdata.io>
2022-06-29 11:01:40 +0200

    PATCH: fix(form): avoid errors due to viewpoints without snapshot

--- src/components/bcf-topic-form/BcfTopicForm.vue
+++ src/components/bcf-topic-form/BcfTopicForm.vue
@@ -278,7 +278,7 @@ export default {
     const viewpointsToUpdate = ref([]);
     const viewpointsToDelete = ref([]);
     const viewpointsToDisplay = computed(() =>
-      viewpoints.value.filter(v => v.snapshot).concat(viewpointsToCreate.value)
+      viewpoints.value.concat(viewpointsToCreate.value).filter(v => v.snapshot)
     );
 
     const loading = ref(false);
