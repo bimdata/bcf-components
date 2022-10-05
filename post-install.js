dad06874c2e7d26f47d3a7cbec34597e8f94994a
NicolasRichel <nicolas@bimdata.io>
2022-10-05 16:16:09 +0200

    PATCH: chore: change postinstall script into prebuild:vue2

--- post-install.js
+++
@@ -1,7 +0,0 @@
-const FILE = "./node_modules/vue-template-compiler/index.js";
-
-const { readFileSync, writeFileSync } = require("fs");
-
-let content = readFileSync(FILE, { encoding: "utf8" });
-content = content.replace(/require\('vue'\)/g, "require('vue2')");
-writeFileSync(FILE, content, { encoding: "utf8" });
