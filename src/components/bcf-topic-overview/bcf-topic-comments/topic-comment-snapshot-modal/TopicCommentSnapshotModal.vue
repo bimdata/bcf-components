b7de0ad77ae49676e8005351af6a962a1d38de69
Gaelle <gaelle@bimdata.io>
2023-02-24 18:10:30 +0100

    create modal for topic comment snapshot

---
+++ src/components/bcf-topic-overview/bcf-topic-comments/topic-comment-snapshot-modal/TopicCommentSnapshotModal.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <img :src="topic.snapshot.snapshot_data" />
+  </div>
+</template>
+
+<script>
+import { inject } from "vue";
+export default {
+  props: {
+    topic: {
+      type: Object,
+    },
+  },
+  setup(props) {
+    inject("$viewer");
+  },
+};
+</script>
