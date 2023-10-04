043d4ecb73eb073c77697918e8c241149f8b5dfb
Gaelle <gaelle@bimdata.io>
2023-10-04 10:02:25 +0200

    delete unused order in config

--- src/config.js
+++ src/config.js
@@ -45,36 +45,30 @@ const VIEWPOINT_TYPES = Object.freeze({
 
 const VIEWPOINT_CONFIG = Object.freeze({
   [VIEWPOINT_TYPES.IFC3D]: {
-    order: 1,
     window: "3d",
     plugin: "viewer3d",
     icon: "fileIfcPolychrome",
   },
   [VIEWPOINT_TYPES.POINT_CLOUD]: {
-    order: 2,
     window: "pointCloud",
     plugin: "pointCloud",
   },
   [VIEWPOINT_TYPES.IFC2D]: {
-    order: 3,
     window: "2d",
     plugin: "viewer2d",
     icon: "fileIfcPolychrome",
   },
   [VIEWPOINT_TYPES.DWG]: {
-    order: 4,
     window: "dwg",
     plugin: "dwg",
     icon: "fileDwgPolychrome",
   },
   [VIEWPOINT_TYPES.PLAN]: {
-    order: 5,
     window: "plan",
     plugin: "plan",
     icon: "filePlanPolychrome",
   },
   [VIEWPOINT_TYPES.PANORAMA]: {
-    order: 6,
     window: "panorama",
     plugin: "panorama",
     icon: "filePlanPolychrome",
