03957927033e04086253c1628603b8fb977dd5bb
NicolasRichel <nicolas@bimdata.io>
2024-02-06 16:19:35 +0100

    PATCH: fix: highlight/unhighlight viewer methods

--- src/utils/viewer.js
+++ src/utils/viewer.js
@@ -1,17 +1,19 @@
 export function getViewerOptions($viewer) {
   return $viewer.globalContext.localContexts
-    .filter(ctx => ctx.viewer)
+    .filter(ctx => ctx.viewer && ctx.loadedModels.length > 0)
     .map((ctx, i) => ({ key: ctx.id, index: i, name: ctx.viewer.$plugin.name, context: ctx }));
 }
 
 export function highlightViewer(context) {
-  context.el.style.boxSizing = "border-box";
-  context.el.style.border = "2px solid var(--color-primary)";
-  context.el.style.opacity = ".85";
+  const el = context.viewer.$viewer.localContext.el;
+  el.style.boxSizing = "border-box";
+  el.style.border = "2px solid var(--color-primary)";
+  el.style.opacity = ".85";
 }
 
 export function unhighlightViewer(context) {
-  context.el.style.boxSizing = "";
-  context.el.style.border = "";
-  context.el.style.opacity = "";
+  const el = context.viewer.$viewer.localContext.el;
+  el.style.boxSizing = "";
+  el.style.border = "";
+  el.style.opacity = "";
 }
